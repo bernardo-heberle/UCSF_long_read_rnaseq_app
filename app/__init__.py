5d907d2b5c73b903bbe3346a5cac29d1660bddbe
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2025-04-28 18:07:08 -0500

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 import atexit
 from app.utils.db_utils import cleanup
-import os
 
 # --- Logging Setup ---
 # Memory tracking removed
@@ -15,8 +14,6 @@ import os
 
 # --- End Scheduler ---
 
-# Get absolute path for assets folder
-assets_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets')
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 # Create Dash application
@@ -24,9 +21,7 @@ app = Dash(
     __name__,
     external_stylesheets=[dbc.themes.COSMO, dbc_css],
     suppress_callback_exceptions=True,
-    assets_folder=assets_path,
-    serve_locally=True,  # Ensure assets are served locally
-    eager_loading=True   # Pre-load all assets at startup
+    assets_folder='assets'
 )
 server = app.server
 
