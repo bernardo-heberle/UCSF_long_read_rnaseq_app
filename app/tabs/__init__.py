3ded5268f4eb1e85adaa4a78050a4ffd461edc2d
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2025-08-30 14:11:48 -0500

    revert test

--- app/tabs/__init__.py
+++ app/tabs/__init__.py
@@ -20,5 +20,3 @@ tabs = {
 
 # This file can be empty, or you can directly import tab modules if needed
 # You could import tab1 here if you want to make it directly available when importing app.tabs
-
-#hi
